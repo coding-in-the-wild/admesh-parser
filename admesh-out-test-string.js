ff09fc45cf94548dd8e0fb36f3ebd20abe26fe99
Joseph Dykstra <josephdykstra@gmail.com>
2014-01-28 15:20:41 -0600

    Inital commit - test string
    this string is a sample output from the admesh application, used for
    testing the parser (admesh-out-to-obj)

---
+++ admesh-out-test-string.js
@@ -0,0 +1,31 @@
+﻿module.exports = "================= Results produced by ADMesh version 0.97.3 =================\
+Input file         : sphere.stl\
+File type          : Binary STL file\
+Header             : Processed by ADMesh version 0.97.3\
+============== Size ==============\
+Min X = -1.334557, Max X = 1.370952\
+Min Y = -1.377953, Max Y = 1.377230\
+Min Z = -1.373225, Max Z = 1.242838\
+========= Facet Status ========== Original ============ Final ====\
+Number of facets                 :  3656                3656\
+Facets with 1 disconnected edge  :    18                   0\
+Facets with 2 disconnected edges :     3                   0\
+Facets with 3 disconnected edges :     0                   0\
+Total disconnected facets        :    21                   0\
+=== Processing Statistics ===     ===== Other Statistics =====\
+Number of parts       :     1        Volume   :  10.889216\
+Degenerate facets     :     0\
+Edges fixed           :    24\
+Facets removed        :     0\
+Facets added          :     0\
+Facets reversed       :     0\
+Backwards edges       :     0\
+Normals fixed         :     0"
+
+
+/* **********helpful things********** *\
+http://nodejs.org/api/stream.html#stream_stream_push
+http://nodejs.org/api/process.html#process_process_stdout
+http://nodejs.org/api/child_process.html#child_process_child_process_exec_command_options_callback
+https://sites.google.com/a/varlog.com/www/admesh-htm
+\* ********************************** */
