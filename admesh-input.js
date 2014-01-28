be3c54c95179e8617b3f01e11f41819713c6eaf8
Joseph Dykstra <josephdykstra@gmail.com>
2014-01-28 15:17:36 -0600

    Initial commit - input
    when ran, admesh-input gets the info that admesh spits out. (Still
    buggy)

---
+++ admesh-input.js
@@ -0,0 +1,16 @@
+﻿var convert = require('admesh-out-to-obj.js')
+var exec = require('child_process').exec,
+    child;
+
+var resultObj = {}
+
+child = exec("echo what's up", //change this to "admesh [options] file.stl"
+	function (error, stdout, stderr) {
+		if (error !== null) {
+			console.log('exec error: ' + error);
+		} else {
+			resultObj = convert(stdout)
+		}
+});
+
+module.exports = resultObj
