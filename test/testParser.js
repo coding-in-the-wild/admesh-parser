1a091a25b1766148a7fa8a1f21c40bc89d72f12e
Joseph Dykstra <josephdykstra@gmail.com>
2014-02-11 14:56:56 -0600

    Merged testParser.js and admeshSampleString.js...

--- test/testParser.js
+++ test/testParser.js
@@ -1,9 +1,31 @@
 var test = require('tap').test
 var parser = require("../admeshStringParser.js")
-var testString = require("./admeshSampleString.js")
 
 test("parser returns correct numbers", function(t) {
-	var obj = parser(testString)
+	var obj = parser(
+		"============ Results produced by ADMesh version 0.97.3 ============\n"+
+		"Input file         : sphere.stl\n"+
+		"File type          : Binary STL file\n"+
+		"Header             : Processed by ADMesh version 0.97.3\n"+
+		"============== Size ==============\n"+
+		"Min X = -1.334557, Max X = 1.370952\n"+
+		"Min Y = -1.377953, Max Y = 1.377230\n"+
+		"Min Z = -1.373225, Max Z = 1.242838\n"+
+		"========= Facet Status ========== Original ============ Final ====\n"+
+		"Number of facets                 :  3656                3656\n"+
+		"Facets with 1 disconnected edge  :    18                   0\n"+
+		"Facets with 2 disconnected edges :     3                   0\n"+
+		"Facets with 3 disconnected edges :     0                   0\n"+
+		"Total disconnected facets        :    21                   0\n"+
+		"=== Processing Statistics ===     ===== Other Statistics =====\n"+
+		"Number of parts       :     1        Volume   :  10.889216\n"+
+		"Degenerate facets     :     4\n"+
+		"Edges fixed           :    24\n"+
+		"Facets removed        :    14\n"+
+		"Facets added          :     3\n"+
+		"Facets reversed       :     2\n"+
+		"Backwards edges       :     0\n"+
+		"Normals fixed         :     12")
 	
 	test("parser gets correct processed version and input name", function (t) {
 		t.equal(obj.processedByVersion,"0.97.3")
@@ -15,9 +37,9 @@ test("parser returns correct numbers", function(t) {
 		t.equal(obj.x.min, -1.334557)
 		t.equal(obj.y.min, -1.377953)
 		t.equal(obj.z.min, -1.373225)
-		t.equal(obj.x.max, 1.370952)
-		t.equal(obj.y.max, 1.377230)
-		t.equal(obj.z.max, 1.242838)
+		t.equal(obj.x.max,  1.370952)
+		t.equal(obj.y.max,  1.377230)
+		t.equal(obj.z.max,  1.242838)
 		t.end()
 	})
 	
