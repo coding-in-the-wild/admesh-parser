bfea9f206f2fd590674e9d8fb7e616435cd6148c
Joseph Dykstra <josephdykstra@gmail.com>
2014-02-11 15:01:24 -0600

    updated and added tests!

--- test/testRunAdmeshOnCompCube.js
+++ test/testRunAdmeshOnCompCube.js
@@ -1,15 +1,7 @@
-var runAdmesh = require("../runAdmesh.js")
+var runAdmesh = require("..\\runAdmesh.js")
 var test = require('tap').test
-
-var admeshDirectory =
-	//"C:\\Users\\Joseph\\Documents\\Github\\admesh-to-js-obj\\"
-	"C:\\Github\\admesh-to-js-obj\\"
-var ccFileAndDirectory =
-	//"C:\\Users\\Joseph\\Documents\\Github\\admesh-to-js-obj\\companion-cube.stl"
-	"C:\\Github\\admesh-to-js-obj\\companion-cube.stl"
-var ccFileAndDirectory2 =
-	//"C:\\Users\\Joseph\\Documents\\Github\\admesh-to-js-obj\\companion-cube-2.stl"
-	"C:\\Github\\admesh-to-js-obj\\companion-cube-2.stl"
+var admeshDir =		"..\\admesh"
+var compCubeDirs =	[".\\companion-cube.stl", ".\\companion-cube-2.stl"]
 
 
 var compareObjects = function(testObj, goodObj) {
@@ -30,10 +22,10 @@ var compareObjects = function(testObj, goodObj) {
 
 
 test("run admesh on companion cube 1", function(t) {
-	var correctValues_cc = {x:{min:12.825}, y:{min:20.82688}, parts:1, volume:0,
-		edges:{backwards:0}, normalsFixed:6}
+	var correctValues_cc = {x:{min:12.825}, y:{min:20.82688}, parts:1,
+		volume:0, edges:{backwards:0}, normalsFixed:6}
 	
-	runAdmesh(admeshDirectory, ccFileAndDirectory, function(err, testObj) {
+	runAdmesh(admeshDir, compCubeDirs[0], function(err, testObj) {
 		t.notOk(err)
 		compareObjects(testObj, correctValues_cc)
 		t.end()
@@ -45,7 +37,7 @@ test("run admesh on companion cube 2", function(t) {
 	var correctValues_cc2 = {x:{min:-21.346926}, y:{min:-25.001492}, parts:7,
 		volume:33.482048, edges:{backwards:0}, normalsFixed:16}
 	
-	runAdmesh(admeshDirectory, ccFileAndDirectory2, function(err, testObj) {
+	runAdmesh(admeshDir, compCubeDirs[1], function(err, testObj) {
 		t.notOk(err)
 		compareObjects(testObj, correctValues_cc2)
 		t.end()
