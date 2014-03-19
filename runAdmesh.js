350836285d89b2c320556b7a1021ca31a7588f79
Joseph Dykstra <josephdykstra@gmail.com>
2014-03-19 15:13:59 -0500

    Cleaned up main file (runAdmesh.js)

--- runAdmesh.js
+++ runAdmesh.js
@@ -1,19 +1,15 @@
 ﻿var convertStrToObj = require("./admeshStringParser.js")
 
 module.exports = function convertAdmesh(admeshDir, options, callback) {
-	var cp = require('child_process')
-	var child
-	var calledBack = false
-	
 	if (typeof options === "string")
 		options = [options]
 
-	child = cp.exec(admeshDir+" "+options.join(" "),
+	require('child_process').exec(admeshDir+" "+options.join(" "),
 		function (error, stdout, stderr) {
 			if (error) {
 				callback(error)
 			} else {
-				callback(0, convertStrToObj(stdout))
+				callback(false, convertStrToObj(stdout))
 			}
 		}
 	)
