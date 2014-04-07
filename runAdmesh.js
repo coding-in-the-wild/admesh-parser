4c9fe74571785d392c14599a45c2becad1295cd2
Joseph Dykstra <josephdykstra@gmail.com>
2014-04-07 14:22:51 -0500

    Cleaned up runAdmesh.js

--- runAdmesh.js
+++ runAdmesh.js
@@ -7,7 +7,9 @@ module.exports = function convertAdmesh(admeshDir, options, callback) {
 	if (!admeshDir)
 		admeshDir = 'C:/Github/admesh-parser/admesh.exe'
 	
+	console.log('cmd '+admeshDir+" "+options.join(" "))
 	require('child_process').exec(admeshDir+" "+options.join(" "), function (err, stdout, stderr) {
+		if (err) console.log('cp error: '+err.message)
 		callback(err, convertStrToObj(stdout))
 	})
 }
