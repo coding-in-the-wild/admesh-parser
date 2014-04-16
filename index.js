f3f5456b3fcedbb592a24c359977f546e50a9078
Joseph Dykstra <josephdykstra@gmail.com>
2014-04-16 09:37:38 -0500

    Cleaned up main file

--- index.js
+++ index.js
@@ -2,16 +2,10 @@
 var cp = require('child_process')
 
 module.exports = function convertAdmesh(admeshDir, options, callback) {
-	if (typeof options === "string")
-		options = [options]
+	if (typeof options === "string") options = [options]
 
-	cp.exec(admeshDir+" "+options.join(" "),
-		function (error, stdout, stderr) {
-			if (error) {
-				callback(error)
-			} else {
-				callback(0, convertStrToObj(stdout))
-			}
-		}
-	)
+	cp.exec(admeshDir+" "+options.join(" "), function (err, stdout) {
+		if (err) callback(err)
+		else     callback(false, convertStrToObj(stdout))
+	})
 }
