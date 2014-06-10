9bc1abd039a94508ee3c9c1a752154cf6d14b003
Joseph Dykstra <josephdykstra@gmail.com>
2014-06-09 22:00:29 -0500

    Prettied up a bit

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ module.exports = function AdmeshParser(admeshDir) {
 	return function admeshParser(options, cb) {
 		if (typeof options === "string") options = [options]
 
-		cp.exec("\""+admeshDir+"\" "+options.join(" "), function (err, stdout) {
+		cp.exec('"'+admeshDir+'" '+options.join(' '), function (err, stdout) {
 			cb(err, parser(stdout))
 		})
 	}
